208ba96f4b8d97a0520c07f448e4e10b00f836f8
jilenloa <jilenloa@gmail.com>
2019-03-13 22:52:19 +0000

    Added Socketcluster channels

---
+++ src/channel/socketcluster-presence-channel.ts
@@ -0,0 +1,36 @@
+import { PresenceChannel } from './presence-channel';
+import {SocketClusterPrivateChannel} from "./socketcluster-private-channel";
+
+/**
+ * This class represents a Socket.io presence channel.
+ */
+export class SocketClusterPresenceChannel extends SocketClusterPrivateChannel implements PresenceChannel {
+    /**
+     * Register a callback to be called anytime the member list changes.
+     */
+    here(callback: Function): SocketClusterPresenceChannel {
+        this.on('presence:subscribed', (members: any[]) => {
+            callback(members.map(m => m.user_info));
+        });
+
+        return this;
+    }
+
+    /**
+     * Listen for someone joining the channel.
+     */
+    joining(callback: Function): SocketClusterPresenceChannel {
+        this.on('presence:joining', (member) => callback(member.user_info));
+
+        return this;
+    }
+
+    /**
+     * Listen for someone leaving the channel.
+     */
+    leaving(callback: Function): SocketClusterPresenceChannel {
+        this.on('presence:leaving', (member) => callback(member.user_info));
+
+        return this;
+    }
+}
