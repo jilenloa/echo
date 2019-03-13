b372e4ae264fd1df5f9672db0033579a1aba7103
jilenloa <jilenloa@gmail.com>
2019-03-13 23:02:20 +0000

    Load the socketcluster chanels

--- src/channel/index.ts
+++ src/channel/index.ts
@@ -9,3 +9,7 @@ export * from './socketio-presence-channel';
 export * from './null-channel';
 export * from './null-private-channel';
 export * from './null-presence-channel';
+
+export * from './socketcluster-channel';
+export * from './socketcluster-private-channel';
+export * from './socketcluster-presence-channel';
